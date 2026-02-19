b35d2a89ec7e5da5d4c967d125dee0eca32c0ba6
PEDRO CAVANI <pedro.cavani@portalsesisp.org.br>
2026-02-19 11:32:46 -0300

    entrega exercicio1, 2, 3

---
+++ exercicio2.cs
@@ -0,0 +1,11 @@
+// -- Arquivo Exercicio2.cs
+public static class Exercicio2
+{
+    private static string nome = "Pedro Cavani";
+    
+    public static void Executar()
+    {
+        Console.WriteLine(nome);
+        Console.WriteLine();
+    }
+}
